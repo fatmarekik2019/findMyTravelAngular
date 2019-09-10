850add1465e3014ccedcd1f9984f5df71b7744e7
fatmarekik2019 <fatma.rekik88@gmail.com>
2019-09-10 16:47:34 +0100

    verification username

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -16,6 +16,7 @@ export class RegisterComponent implements OnInit {
     birthday: new FormControl('1900-01-01'),
     password: new FormControl('')
   });
+  result = true;
   constructor(private registerService: RegisterService) { }
 
   ngOnInit() {
@@ -27,6 +28,15 @@ export class RegisterComponent implements OnInit {
     console.log( this.registerClient.value);
     this.registerService.registerClient(this.registerClient.value).subscribe( (res: any)=>{
       console.log(res);
+    });
+  }
+  
+  verifUserName(e){
+    this.registerService.verifUserName(e.target.value).subscribe((res: any)=>
+    {
+      console.log (e.target.value);
+      console.log(res);
+      this.result = res;
     });
   }
 
