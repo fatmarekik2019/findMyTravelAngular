1779e1cdf9f2a7eb754f32541c66ad5b6c82b96f
fatmarekik2019 <fatma.rekik88@gmail.com>
2019-09-17 21:45:39 +0100

    forgot password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { RegisterService } from '../register.service';
+import { PasswordService} from '../password.service';
 
 @Component({
   selector: 'app-login',
@@ -14,8 +15,19 @@ export class LoginComponent implements OnInit {
     userName: new FormControl(''),
     password: new FormControl('')
   });
+  forgotPassword = new FormGroup({
+    
+    email: new FormControl("youremail@gmail.com",
+       Validators.compose([
+         Validators.required,
+         Validators.pattern("[A-Za-z0-9.%-]+@[A-Za-z0-9.%-]+\\.[a-z]{2,3}")
+    
+     ])),
+     userName : new FormControl('')
+  });
   result : true;
-  constructor(private loginService: LoginService, private registerService: RegisterService) { }
+  datas : any;
+  constructor(private loginService: LoginService, private registerService: RegisterService, private passwordService: PasswordService) { }
 
   ngOnInit() {
   }
@@ -31,9 +43,16 @@ console.log(this.login.value);
   verifUserName(e){
     this.registerService.verifUserName(e.target.value).subscribe((res: any)=>
     {
-      //console.log(res);
+      
       this.result = res;
     });
   }
+
+  onClickSend(){
+    console.log(this.forgotPassword.value);
+    this.passwordService.forgotPassword(this.forgotPassword.value.email, this.forgotPassword.value).subscribe((res : any)=>
+    {console.log(res);
+  });
+}
 
 }
